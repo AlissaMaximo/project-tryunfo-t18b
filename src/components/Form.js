76e8f4106e248d76ac2d43317fc58e4de6fb1893
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-02-09 02:45:35 -0300

    Requisito 1 completo.

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,18 @@ class Form extends Component {
     return (
       <form>
         <input type="text" data-testid="name-input" />
+        <textarea data-testid="description-input" />
+        <input type="number" data-testid="attr1-input" />
+        <input type="number" data-testid="attr2-input" />
+        <input type="number" data-testid="attr3-input" />
+        <input type="text" data-testid="image-input" />
+        <select data-testid="rare-input">
+          <option value="normal">Normal</option>
+          <option value="raro">Raro</option>
+          <option value="muito raro">Muito Raro</option>
+        </select>
+        <input type="checkbox" data-testid="trunfo-input" id="trunfo" value="trunfo" />
+        <button type="button" data-testid="save-button">Salvar</button>
       </form>
     );
   }
